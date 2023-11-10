34aa454863220f32c7375e2b905660de06199983
MTL\ldunphy <liam.dunphy@mymail.champlain.edu>
2023-11-09 21:50:53 -0500

    Balancing

--- Assets/Scripts/UI/Buttons.cs
+++ Assets/Scripts/UI/Buttons.cs
@@ -9,4 +9,9 @@ public class Buttons : MonoBehaviour
     {
         SceneManager.LoadScene("MathMenu");
     }
+
+    public void LevelOne()
+    {
+        SceneManager.LoadScene("BattleField");
+    }
 }
